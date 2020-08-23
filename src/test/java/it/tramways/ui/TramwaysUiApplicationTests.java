80b7dfe2d3495b013f8d1c47af58eef7955f86d0
naigo <lorenzo.niccolai94@gmail.com>
2020-08-23 22:42:38 +0200

    First commit

---
+++ src/test/java/it/tramways/ui/TramwaysUiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.tramways.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TramwaysUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
